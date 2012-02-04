7ab1624d68e933b5c260ebf153439c36159470aa
Jerome Eteve <jerome@eteve.net>
2012-02-04 13:07:55 +0000

    Adding missing default_filters 'is' property. This cause the test to break with Mason 2.15 and Moose 2.02

--- lib/Mason/Plugin/DefaultFilter/Interp.pm
+++ lib/Mason/Plugin/DefaultFilter/Interp.pm
@@ -4,6 +4,6 @@ use Mason::PluginRole;
 
 # VERSION
 
-has default_filters => ( isa => 'ArrayRef', default => sub { [] } );
+has default_filters => ( isa => 'ArrayRef', default => sub { [] } , is => 'rw' );
 
 1;
